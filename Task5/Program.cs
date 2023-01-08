5c00e022d4b18a8a2469122ed4e486e06c8cf4ba
ElizavetaPonkratova <elizaveta.ski@yandex.ru>
2023-01-08 19:45:37 +0700

    HWSem8-5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,8 +1,12 @@
-﻿
+﻿/*Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07*/
+
 int size = ReadInt("Введите размер массива: ");
-
 int[,] nums = new int[size, size];
-
 Console.WriteLine($"Дан массив размером {size} x {size}");
 
 int num = 1;
@@ -24,7 +28,6 @@ while (num <= size * size)
 }
 
 Print2DArray(nums);
-
 void Print2DArray(int[,] array2D)
 {
     for (int i = 0; i < array2D.GetLength(0); i++)
